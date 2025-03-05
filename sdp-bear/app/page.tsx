ddff7ddb601dda3caa2ef2a0e6be3676738ce630
Ashley <ashley.jacob@uconn.edu>
2025-03-05 10:07:02 -0500

    Basic setup

---
+++ sdp-bear/app/page.tsx
@@ -0,0 +1,18 @@
+import "./login.css";
+
+export default function Login() {
+  return (
+
+      <div className="login">
+          <input
+           type="text"
+           placeholder="Username"
+           />
+           <input
+           type="text"
+           placeholder="Password"
+           />
+      </div>
+
+  );
+}
