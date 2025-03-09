80948b6b48032b3d687612ee26e8420475c3c72d
ashley.jacob@uconn.edu <ashley.jacob@uconn.edu>
2025-03-09 17:39:38 -0400

    Added page for detections.

--- sdp-bear/app/page.tsx
+++ sdp-bear/app/page.tsx
@@ -1,17 +1,24 @@
-import "./login.css";
+import "./main.css";
+import Bear from "./resources/beartest1.jpg";
+import Image, {StaticImageData} from "next/image";
 
 export default function Login() {
   return (
-
-      <div className="login">
-          <input
-           type="text"
-           placeholder="Username"
-           />
-           <input
-           type="text"
-           placeholder="Password"
-           />
+      <div className="main">
+        <div className="title">
+          <span>Bear Detections</span>
+        </div>
+        <div className="mainImage">
+          <img className="bigImg" src={Bear.src} alt="hello"></img>
+        </div>
+        <div className="detections">
+          <img src={Bear.src} alt="hello"></img>
+          <img src={Bear.src} alt="hello"></img>
+          <img src={Bear.src} alt="hello"></img>
+          <img src={Bear.src} alt="hello"></img>
+          <img src={Bear.src} alt="hello"></img>
+          <img src={Bear.src} alt="hello"></img>
+        </div>
       </div>
 
   );
